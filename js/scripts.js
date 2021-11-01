9d22c7b9dda9c08dc9eedd7a4e975c38dee99d69
Vitaliy Sulimenko <prevetal@gmail.com>
2021-11-01 14:56:12 +0200

    Фикс. шапка при скроллинге

--- js/scripts.js
+++ js/scripts.js
@@ -323,6 +323,49 @@ $(window).resize(() => {
 
 
 
+$(window).on('load', () => {
+	// Фикс. шапка
+	headerInit = true,
+		headerHeight = $('header').outerHeight()
+
+	$('header').wrap('<div class="header_wrap"></div>')
+	$('.header_wrap').height(headerHeight)
+
+	headerInit && $(window).scrollTop() > headerHeight
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+})
+
+
+
+$(window).resize(() => {
+	// Фикс. шапка
+	headerInit = false
+	$('.header_wrap').height('auto')
+
+	setTimeout(() => {
+		headerInit = true
+		headerHeight = $('header').outerHeight()
+
+		$('.header_wrap').height(headerHeight)
+
+		headerInit && $(window).scrollTop() > headerHeight
+			? $('header').addClass('fixed')
+			: $('header').removeClass('fixed')
+	}, 100)
+})
+
+
+
+$(window).scroll(() => {
+	// Фикс. шапка
+	typeof headerInit !== 'undefined' && headerInit && $(window).scrollTop() > headerHeight
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+})
+
+
+
 // Выравнивание товаров
 function productHeight(context, step) {
 	let start = 0,
